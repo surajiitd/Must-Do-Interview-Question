c4b353f188d41232cd2ed89922b5f919f729a8b2
Suraj Patni <patnisrj22@gmail.com>
2020-02-18 21:10:52 +0530

    bin search by recursion

--- array/binary_search.cpp
+++ array/binary_search.cpp
@@ -1,15 +1,11 @@
 // https://practice.geeksforgeeks.org/problems/binary-search/1/?track=md-arrays&batchId=144
-// by iteration.
+// by recursion.
 int bin_search(int A[], int left, int right, int k)
 {
-    while(left<=right)
-    {
-        int mid = (left+right)/2;
-        if(A[mid]==k)   return mid;
-        else if(A[mid]>k)   right = mid-1;
-        else    left = mid+1;
-        
-    }
-    return -1;
+    if(left>right)  return -1;
+    int mid = (left+right)/2;
+    if(A[mid]==k)   return mid;
+    else if(A[mid]>k)   return bin_search(A,left,mid-1,k);
+    else    return bin_search(A,mid+1,right,k);
     
 }
